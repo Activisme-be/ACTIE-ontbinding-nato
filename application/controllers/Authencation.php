b710ff2d315af8760090d1a607e58bce1482c1d8
Tjoosten <Tjoosten3@gmail.com>
2017-05-28 01:44:08 +0200

    Implement verifylogin method

--- application/controllers/Authencation.php
+++ application/controllers/Authencation.php
@@ -12,7 +12,7 @@ class Authencation extends MY_Controller
     public function __construct() 
     {
         parent::__construct(); 
-        $this->load->library(['blade', 'session']);
+        $this->load->library(['blade', 'session', 'form_validation']);
         $this->load->helper(['url']);
 
         $this->user        = $this->session->userdata('user');
@@ -29,5 +29,28 @@ class Authencation extends MY_Controller
     {
         $data['title'] = 'Inloggen';
         return $this->blade->render('authencation/index', $data);
+    }
+
+    /**
+     * Base method for validation the credentials.
+     *
+     * @return Response|Blade view
+     */
+    public function verify() 
+    {
+        $this->form_validation->set_rules('email', 'email', 'trim|required'); 
+        $this->form_validation->set_rules('password', 'password', 'trim|required|callback_check_database');
+
+        if ($this->form_validation->run() === false) { // Form validation fails
+            $data['title'] = 'Inloggen';
+            return $this->blade->render('authencation/index', $data);
+
+            $this->session->set_flashdata('class', 'alert alert-danger');
+            $this->session->set_flashdata('message', 'De gebruikersnaam en het wachtwoord is foutief. En kan niet worden gevonden.');
+
+            return $this->blade->render('authencation/index', $data);
+        }
+
+        return redirect(site_url('backend'));
     }
 }
