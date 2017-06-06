bf5e9842b9eeb338250952c6a5329257b03bae29
Tim Joosten <Tjoosten3@gmail.com>
2017-06-06 21:06:10 +0200

    Reformat application language file

--- application/language/dutch/application_lang.php
+++ application/language/dutch/application_lang.php
@@ -1,98 +1,99 @@
-<?php 
+<?php
 
 // Social media
-$lang['share_twitter']   = 'Deel op Twitter';
-$lang['share_facebook']  = 'Deel op Facebook';
-$lang['share_google']    = 'Deel op Google+';
-$lang['share_linkedin']  = 'Deel op LinkedIn';
+$lang['share_twitter']   							= 'Deel op Twitter';
+$lang['share_facebook']  							= 'Deel op Facebook';
+$lang['share_google']    							= 'Deel op Google+';
+$lang['share_linkedin']  							= 'Deel op LinkedIn';
 
-// Signature form - misc. 
-$lang['form_heading'] = 'Ik steun dit verdrag:';
+// Signature form - misc.
+$lang['form_heading'] 								= 'Ik steun dit verdrag:';
 
 // Signature form - buttons
-$lang['form_button_submit'] = 'Teken'; 
-$lang['form_button_reset']  = 'Formulier legen';
+$lang['form_button_submit'] 						= 'Teken';
+$lang['form_button_reset']  						= 'Formulier legen';
 
 // Signature form - disclaimer
-$lang['form_disclaimer_notice'] = 'Bij het steunen van dit verdrag. Gaat u akkoord met de';
-$lang['form_disclaimer_link_text'] = 'disclaimer';
+$lang['form_disclaimer_notice'] 					= 'Bij het steunen van dit verdrag. Gaat u akkoord met de';
+$lang['form_disclaimer_link_text'] 					= 'disclaimer';
 
 // Signature form - checkboxes
-$lang['form_sign_anonymous'] = 'Ik teken anoniem.';
+$lang['form_sign_anonymous'] 						= 'Ik teken anoniem.';
 
 // Signature form - selects
-$lang['form_select_country'] = 'Selecteer je land';
+$lang['form_select_country'] 						= 'Selecteer je land';
 
-// Signature form - placeholders 
-$lang['placeholder_postal_code'] = 'Code';
-$lang['placeholder_city_name']   = 'Stadsnaam';
-$lang['placeholder_email']       = 'Email adres';
-$lang['placeholder_name'] = 'Naam en voornaam';
+// Signature form - placeholders
+$lang['placeholder_postal_code'] 					= 'Code';
+$lang['placeholder_city_name']   					= 'Stadsnaam';
+$lang['placeholder_email']       					= 'Email adres';
+$lang['placeholder_name'] 							= 'Naam en voornaam';
 
 // Support section.
-$lang['support_heading'] = 'Steunbetuigingen';
-$lang['support_no_signatures'] = 'Er zijn nog geen steunbetuigingen voor dit verdrag.';
-$lang['support_with_signatures'] = 'Er zijn %1$s steunbetuigingen voor dit verdrag.';
-$lang['support_alert_status'] = 'Info';
-$lang['support_table_name'] = 'Naam';
-$lang['support_table_country'] = 'Land';
-$lang['support_table_place'] = 'Plaats';
-$lang['support_table_date'] = 'Datum';
-$lang['support_table_anonymous'] = 'De gebruiker heeft gekozen om anoniem te tekenen.';
+$lang['support_heading'] 							= 'Steunbetuigingen';
+$lang['support_no_signatures'] 						= 'Er zijn nog geen steunbetuigingen voor dit verdrag.';
+$lang['support_with_signatures'] 					= 'Er zijn %1$s steunbetuigingen voor dit verdrag.';
+$lang['support_alert_status'] 						= 'Info';
+$lang['support_table_name'] 						= 'Naam';
+$lang['support_table_country'] 						= 'Land';
+$lang['support_table_place']				 		= 'Plaats';
+$lang['support_table_date'] 						= 'Datum';
+$lang['support_table_anonymous'] 					= 'De gebruiker heeft gekozen om anoniem te tekenen.';
 
 // Social media meta data
-$lang['meta_facebook_title'] = 'Ontbind de NATO!';
-$lang['meta_facebook_discription'] = 'We verklaren dat de NAVO verouderd is. Het Warschaupacts is meer dan een kwart eeuw geleden ontbonden.
+$lang['meta_facebook_title'] 						= 'Ontbind de NATO!';
+$lang['meta_facebook_discription'] 					= 'We verklaren dat de NAVO verouderd is. Het Warschaupacts is meer dan een kwart eeuw geleden ontbonden.
         Het is tijd dat ook de NAVO ophoudt te bestaan en dat we onze veiligheid gemeenschappelijk, samen met onze buren,
         maar ook met landen uit de hele wereld, organiseren. We willen dit doen in de schoot van de Verenigde Naties, die net daarvoor een mandaat heeft gekregen.';
 
-$lang['meta_twitter_title'] = 'Onbind de NATO!';
-$lang['meta_twitter_description'] = 'We verklaren dat de NAVO verouderd is. Het Warschaupactss is meer dan een kwart eeuw geleden ontbonden.
+$lang['meta_twitter_title'] 						= 'Onbind de NATO!';
+$lang['meta_twitter_description'] 					= 'We verklaren dat de NAVO verouderd is. Het Warschaupactss is meer dan een kwart eeuw geleden ontbonden.
         Het is tijd dat ook de NAVO ophoudt te bestaan en dat we onze veiligheid gemeenschappelijk, samen met onze buren,
         maar ook met landen uit de hele wereld, organiseren. We willen dit doen in de schoot van de Verenigde Naties, die net daarvoor een mandaat heeft gekregen.';
 
 // Application layout
-$lang['app_name'] = 'Ontbind de NATO';
+$lang['app_name'] 									= 'Ontbind de NATO';
 
 // Navigation
-$lang['nav_text'] = 'Besluitvorming';
-$lang['nav_support'] = 'Steunbetuigingen';
-$lang['nav_disclaimer'] = 'Disclaimer';
+$lang['nav_text'] 									= 'Besluitvorming';
+$lang['nav_support'] 								= 'Steunbetuigingen';
+$lang['nav_disclaimer'] 							= 'Disclaimer';
 
 // Navigation - Language
-$lang['nav_lang_dropdown'] = 'Taal';
-$lang['nav_lang_current']  = 'NL';
+$lang['nav_lang_dropdown'] 							= 'Taal';
+$lang['nav_lang_current']  							= 'NL';
 
 // Navigation - Language dropdown
-$lang['nav_lang_english'] = 'Engels';
-$lang['nav_lang_french']  = 'Frans';
-$lang['nav_lang_dutch']   = 'Nederlands';
+$lang['nav_lang_english']						 	= 'Engels';
+$lang['nav_lang_french']  							= 'Frans';
+$lang['nav_lang_dutch']   							= 'Nederlands';
 
 // Authencation - login
-$lang['auth_panel_heading'] = 'Inloggen';
-$lang['auth_label_email']   = 'E-mail';
-$lang['auth_placeholder_email'] = 'E-mail adres';
-$lang['auth_label_password'] = 'Wachtwoord';
-$lang['auth_placeholder_password'] = 'Wachtwoord';
-$lang['auth_button_submit'] = 'Inloggen';
-$lang['auth_button_lost']   = 'Wachtwoord vergeten?';
+$lang['auth_panel_heading'] 						= 'Inloggen';
+$lang['auth_label_email']   						= 'E-mail';
+$lang['auth_placeholder_email'] 					= 'E-mail adres';
+$lang['auth_label_password'] 						= 'Wachtwoord';
+$lang['auth_placeholder_password'] 					= 'Wachtwoord';
+$lang['auth_button_submit'] 						= 'Inloggen';
+$lang['auth_button_lost']   						= 'Wachtwoord vergeten?';
 
 // Controller translations.
-$lang['auth_flash_err_validation'] = 'De gebruikersnaam en het wachtwoord is foutief. En kan niet worden gevonden.';
-$lang['auth_flash_err_no_perm']    = 'U hebt geen rechten om hier in te loggen';
-$lang['auth_flash_err_wrong_combo'] = 'Wrong credentials given.';
-$lang['auth_login_title'] = 'Inloggen';
-$lang['title_disclaimer'] = 'Disclaimer';
-$lang['title_support'] = 'Steunbetuigingen';
-$lang['flash_success_signature'] = 'Bedankt voor het steunen van dit verdrag.';
-$lang['title_index'] = 'Ontbind de NATO!';
+$lang['auth_flash_err_validation'] 					= 'De gebruikersnaam en het wachtwoord is foutief. En kan niet worden gevonden.';
+$lang['auth_flash_err_no_perm']    					= 'U hebt geen rechten om hier in te loggen';
+$lang['auth_flash_err_wrong_combo'] 				= 'Wrong credentials given.';
+$lang['auth_login_title']				 			= 'Inloggen';
+$lang['title_disclaimer'] 							= 'Disclaimer';
+$lang['title_support'] 								= 'Steunbetuigingen';
+$lang['flash_success_signature'] 					= 'Bedankt voor het steunen van dit verdrag.';
+$lang['title_index'] 								= 'Ontbind de NATO!';
 
 // Controller - form-validation
-$lang['validation_auth_login_email'] = 'E-mail adres';
-$lang['validation_auth_login_password'] = 'Wachtwoord';
+$lang['validation_auth_login_email'] 				= 'E-mail adres';
+$lang['validation_auth_login_password'] 			= 'Wachtwoord';
 
-$lang['validation_support_name'] = 'Naam';
-$lang['validation_support_email']   = 'E-mail adres';
-$lang['validation_support_country'] = 'Land';
-$lang['validation_support_city_name'] = 'Stadsnaam';
-$lang['validation_support_postal_code'] = 'Postcode';
+$lang['validation_support_name'] 					= 'Naam';
+$lang['validation_support_email']   				= 'E-mail adres';
+$lang['validation_support_country'] 				= 'Land';
+$lang['validation_support_city_name'] 				= 'Stadsnaam';
+$lang['validation_support_postal_code'] 			= 'Postcode';
+
---
+++ application/language/english/application_lang.php
@@ -0,0 +1,94 @@
+<?php
+
+// Social media
+$lang['share_twitter']   							= '';
+$lang['share_facebook']  							= '';
+$lang['share_google']    							= '';
+$lang['share_linkedin']  							= '';
+
+// Signature form - misc.
+$lang['form_heading'] 								= '';
+
+// Signature form - buttons
+$lang['form_button_submit'] 						= '';
+$lang['form_button_reset']  						= '';
+
+// Signature form - disclaimer
+$lang['form_disclaimer_notice'] 					= '';
+$lang['form_disclaimer_link_text'] 					= '';
+
+// Signature form - checkboxes
+$lang['form_sign_anonymous'] 						= '';
+
+// Signature form - selects
+$lang['form_select_country'] 						= '';
+
+// Signature form - placeholders
+$lang['placeholder_postal_code'] 					= '';
+$lang['placeholder_city_name']   					= '';
+$lang['placeholder_email']       					= '';
+$lang['placeholder_name'] 							= '';
+
+// Support section.
+$lang['support_heading'] 							= '';
+$lang['support_no_signatures'] 						= '';
+$lang['support_with_signatures'] 					= '';
+$lang['support_alert_status'] 						= '';
+$lang['support_table_name'] 						= '';
+$lang['support_table_country'] 						= '';
+$lang['support_table_place']				 		= '';
+$lang['support_table_date'] 						= '';
+$lang['support_table_anonymous'] 					= '';
+
+// Social media meta data
+$lang['meta_facebook_title'] 						= '';
+$lang['meta_facebook_discription'] 					= '';
+
+$lang['meta_twitter_title'] 						= '';
+$lang['meta_twitter_description'] 					= '';
+
+// Application layout
+$lang['app_name'] 									= '';
+
+// Navigation
+$lang['nav_text'] 									= '';
+$lang['nav_support'] 								= '';
+$lang['nav_disclaimer'] 							= '';
+
+// Navigation - Language
+$lang['nav_lang_dropdown'] 							= '';
+$lang['nav_lang_current']  							= '';
+
+// Navigation - Language dropdown
+$lang['nav_lang_english']						 	= '';
+$lang['nav_lang_french']  							= '';
+$lang['nav_lang_dutch']   							= '';
+
+// Authencation - login
+$lang['auth_panel_heading'] 						= '';
+$lang['auth_label_email']   						= '';
+$lang['auth_placeholder_email'] 					= '';
+$lang['auth_label_password'] 						= '';
+$lang['auth_placeholder_password'] 					= '';
+$lang['auth_button_submit'] 						= '';
+$lang['auth_button_lost']   						= '';
+
+// Controller translations.
+$lang['auth_flash_err_validation'] 					= '';
+$lang['auth_flash_err_no_perm']    					= '';
+$lang['auth_flash_err_wrong_combo'] 				= '';
+$lang['auth_login_title']				 			= '';
+$lang['title_disclaimer'] 							= '';
+$lang['title_support'] 								= '';
+$lang['flash_success_signature'] 					= '';
+$lang['title_index'] 								= '';
+
+// Controller - form-validation
+$lang['validation_auth_login_email'] 				= '';
+$lang['validation_auth_login_password'] 			= '';
+
+$lang['validation_support_name'] 					= '';
+$lang['validation_support_email']   				= '';
+$lang['validation_support_country'] 				= '';
+$lang['validation_support_city_name'] 				= '';
+$lang['validation_support_postal_code'] 			= '';
